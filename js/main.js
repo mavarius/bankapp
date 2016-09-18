cd86f514139af979190117f2935f1649655b96f7
mavarius <metalwhirlwind@yahoo.com>
2016-09-17 19:37:27 -0700

    It is not completely broken

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
   // TODO: Balance updates with edit and delete
   // DONE: User can see when transactions were made
 // EXTRA FEATURES LEVEL 1
-  // ODNE: User can give transactions discriptions
+  // DONE: User can give transactions discriptions
   // TODO: User can view who they paid/received money from
   // TODO: User can sort transactions
   // TODO: User can filter transactions
@@ -23,22 +23,32 @@ const App = React.createClass({
   getInitialState() {
     return {
       transactions: [],
-      currentBalance: 0
+      currentBalance: 0,
+      currentlyEditing: {},
+      toUpdateWith: {}
     }
   },
 
   // TRANSACTION ACTION: SAVE
   saveTransaction(transaction) {
-    const { transactions } = this.state;
+    const { transactions, currentlyEditing } = this.state;
 
-    this.setState({
-      transactions: [...transactions, transaction]
-    })
-  },
+    let current = [];
+    current = transactions.filter(transaction => transaction.id === currentlyEditing.id)
 
-  // TRANSACTION ACTION: EDIT
-  editTransaction(id) {
-    const { transactions } = this.state;
+    let update = [];
+
+    if (current.length === 0) {
+      update = [...transactions, transaction]
+    } else {
+      console.log('existing')
+    }
+
+      this.setState({
+        transactions: update,
+        currentlyEditing: {},
+        toUpdateWith: {}
+      })
   },
 
   // TRANSACTION ACTION: DELETE
@@ -50,14 +60,44 @@ const App = React.createClass({
     })
   },
 
+  updateForm(event) {
+    const { toUpdateWith } = this.state;
+    event.preventDefault();
+    let key = event.target.name;
+    let value = event.target.value;
+    toUpdateWith[key] = value;
+    let toUpdate = Object.assign({}, this.props.currentlyEditing, toUpdateWith);
+    this.setState({
+      currentlyEditing: toUpdate
+    });
+  },
+
+  cancelForm(event) {
+    this.setState({
+      currentlyEditing: {},
+      toUpdateWith: {}
+    })
+  },
+
+  // TRANSACTION ACTION: EDIT
+  editTransaction(id) {
+    const { transactions } = this.state;
+
+    let current = transactions.filter(transaction => transaction.id === id)
+
+    this.setState({
+      currentlyEditing: current[0],
+      toUpdateWith: current[0]
+    })
+
+  },
+
   // HANDLE BALANCE UPDATES
   updateBalance(transactionAmount) {
     const { currentBalance } = this.state;
 
-    let newBalance = parseFloat(currentBalance) + parseFloat(transactionAmount)
-
     this.setState({
-      currentBalance: newBalance
+      currentBalance: transactionAmount
     })
   },
 
@@ -87,7 +127,7 @@ const App = React.createClass({
                 <h4 className="modal-title" id="myModalLabel">Transaction Details</h4>
               </div>
               <div className="modal-body">
-                <TransactionForm saveTransaction={this.saveTransaction} updateBalance={this.updateBalance} currentBalance={currentBalance}/>
+                <TransactionForm updateForm={this.updateForm} cancelForm={this.cancelForm} currentlyEditing={this.state.currentlyEditing} saveTransaction={this.saveTransaction} editTransaction={this.editTransaction} updateBalance={this.updateBalance} currentBalance={currentBalance}/>
               </div>
             </div>
           </div>
@@ -123,7 +163,7 @@ const AccountDetails = props => {
             <td className="entryDetails">{transaction.details}</td>
             <td className="entryAmount">${transaction.amount}</td>
             <td className="entryBalance">${transaction.balance}</td>
-            <td className="entryEdit"><button onClick={editTransaction.bind(null, transaction.id)} className="btn btn-sm btn-warning">edit</button></td>
+            <td className="entryEdit"><button onClick={editTransaction.bind(null, transaction.id)} className="btn btn-sm btn-warning" data-toggle="modal" data-target="#transactionFormModal">edit</button></td>
             <td className="entryDelete"><button onClick={deleteTransaction.bind(null, transaction.id)} className="btn btn-sm btn-danger">X</button></td>
           </tr>
         ))}
@@ -136,67 +176,85 @@ const TransactionForm = React.createClass({
   submitForm(e) {
     e.preventDefault();
 
-    const { currentBalance } = this.props;
+    const { currentBalance, transactions, currentlyEditing } = this.props;
 
-    let { details, amount, balance, credit, debit } = this.refs;
+    let { details, amount, balance, debit } = this.refs;
+    let transaction = {};
+    let newAmount;
 
-    let newDate = moment().format('MMM DD, YYYY');
+    if (!currentlyEditing.id) {
+      let newDate = moment().format('MMM DD');
+      let newID = uuid();
 
-    let newID = uuid();
+      newAmount = parseFloat(amount.value)
+      debit.checked ? newAmount *= -1 : newAmount
 
-    if (debit.checked) {
-      amount.value = parseFloat(amount.value) * -1
+      transaction = {
+        id: newID,
+        date: newDate,
+        details: details.value,
+        amount: amount.value,
+        debit: debit.checked
+      }
+    } else {
+      transaction = {
+        id: currentlyEditing.id,
+        date: currentlyEditing.date,
+        details: details.value,
+        amount: amount.value,
+        debit: debit.checked
+      }
     }
 
-    let transaction = {
-      id: newID,
-      date: newDate,
-      details: details.value,
-      amount: amount.value,
-      balance: currentBalance + parseFloat(amount.value),
-      credit: credit.value,
-      debit: debit.value
-    }
-
-    this.setState
+    let newBalance = parseFloat((currentBalance + newAmount).toFixed(2))
+    transaction.balance = newBalance
 
     this.props.saveTransaction(transaction)
-    this.props.updateBalance(transaction.amount)
+    this.props.updateBalance(newBalance)
   },
 
   render() {
+    let details, amount, debit;
+    // console.log("this.props.currentlyEditing: ", this.props.currentlyEditing);
+    if (this.props.currentlyEditing) {
+      details = this.props.currentlyEditing.details;
+      amount = this.props.currentlyEditing.amount;
+      debit = this.props.currentlyEditing.debit;
+    }
+
     return (
       <form id="transactionForm">
+
         <div className="form-group row">
-          <label htmlFor="trDetails" className="col-sm-2 col-form-label col-form-label-sm">Details</label>
+          <label htmlFor="trDetails" className="col-sm-2 col-form-label">Details</label>
           <div className="col-sm-10">
-            <input type="text" ref="details" className="form-control form-control-sm" id="trDetails" placeholder="Hell &amp; Co"/>
+            <input type="text" ref="details" value={details || ""} name="details" onChange={this.props.updateForm} className="form-control form-control-sm" id="trDetails" placeholder="Hell &amp; Co"/>
           </div>
         </div>
+
         <div className="form-group row">
           <label htmlFor="trAmount" className="col-sm-2 col-form-label col-form-label-sm">Amount</label>
           <div className="col-sm-10">
-            <p>$</p><input type="number" ref="amount" className="form-control form-control-sm" id="trAmount" placeholder="0.00" min="0" step="0.01" required/>
+            <input type="number" ref="amount" value={amount || ""} name="amount" onChange={this.props.updateForm} className="form-control form-control-sm" id="trAmount" placeholder="0.00" min="0" step="0.01" required/>
           </div>
         </div>
-        <fieldset className="form-group row">
-        <div className="form-check">
-          <label className="form-check-label">
-            <input className="form-check-input" ref="credit" type="radio" name="radios" id="credit"/>
-            Credit
-          </label>
-        </div>
-        <div className="form-check">
-          <label className="form-check-label">
-            <input className="form-check-input" ref="debit" type="radio" name="radios" id="debit"/>
-            Debit
-          </label>
-        </div>
-      </fieldset>
+
+        <fieldset className="form-group">
+          <div className="btn-group" data-toggle="buttons">
+            <label className="btn btn-primary active">
+              credit<input className="form-check-input" ref="credit" type="radio" name="radios" id="credit"/>
+            </label>
+            <label className="btn btn-primary">
+              debit<input className="form-check-input" ref="debit" value={debit} name="debit" onChange={this.props.updateForm} type="radio" name="radios" id="debit"/>
+            </label>
+          </div>
+        </fieldset>
+
         <div className="formButtons">
-          <button type="button" id="cancelForm" className="btn btn-default" data-dismiss="modal">Cancel</button>
+          <button type="button" onClick={this.props.cancelForm} className="btn btn-default" data-dismiss="modal">Cancel</button>
           <button type="button" onClick={this.submitForm} id="saveForm" className="btn btn-success" data-dismiss="modal">Save</button>
         </div>
+
       </form>
       )
     }
